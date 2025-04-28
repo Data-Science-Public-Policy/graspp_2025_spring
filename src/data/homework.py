530d59a195fbc0a29d448f1489564bc8a3e25bfa
YIJIN-1 <geikinou@gmail.com>
2025-04-28 11:23:26 +0900

    test

--- src/data/homework.py
+++ src/data/homework.py
@@ -1,4 +1,4 @@
- import pandas as pd
+import pandas as pd
 import requests
 import io 
 import os
@@ -6,4 +6,9 @@ import os
 url_iadb = 'https://data.iadb.org/file/download/621615a3-45d5-4c91-99bf-2d4dbee14b8e'
 df_iadb = pd.read_csv(url_iadb)
 
-print (df_iadb.head())
+print (df_iadb['country_id'])
+
+url_iadb2 = 'https://data.iadb.org/file/download/fe4f3683-647c-44f4-a17c-022f22a922a8'
+df_iadb2 = pd.read_stata(url_iadb2)
+
+print(df_iadb2['isocode'])
